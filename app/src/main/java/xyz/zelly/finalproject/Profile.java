ee41191fd643dc4f0c7258fb959cfe009dad394c
zeleney30 <michaelzeleney@gmail.com>
2024-12-02 10:12:11 -0500

    Profile page button functionality

--- app/src/main/java/xyz/zelly/finalproject/Profile.java
+++ app/src/main/java/xyz/zelly/finalproject/Profile.java
@@ -1,15 +1,25 @@
 package xyz.zelly.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
+    private Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.profile_page);
+
+        backButton = findViewById(R.id.buttonBack);
+
+        backButton.setOnClickListener(e -> {
+            Intent intent = new Intent(this, LandingPage.class);
+        });
     }
 }
