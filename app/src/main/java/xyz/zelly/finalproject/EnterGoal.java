eb293052f2964bafa6a914ef59ba2a7e758b3028
zeleney30 <michaelzeleney@gmail.com>
2024-12-03 21:37:07 -0500

    Fix menu button on enter goal page

--- app/src/main/java/xyz/zelly/finalproject/EnterGoal.java
+++ app/src/main/java/xyz/zelly/finalproject/EnterGoal.java
@@ -1,5 +1,6 @@
 package xyz.zelly.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -7,10 +8,19 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EnterGoal extends AppCompatActivity {
+    private Button menuButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.goal_page);
+        
+        menuButton = findViewById(R.id.buttonMenu1);
+        
+        menuButton.setOnClickListener(e -> {
+            Intent intent = new Intent(this, MenuPage.class);
+            startActivity(intent);
+        });
     }
 }
